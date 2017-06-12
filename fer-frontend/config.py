5c19868b91456d83be75f4d6f5e40fe6c5f393b5
Chester Chu <chester.yw.chu@gmail.com>
2017-06-12 17:52:13 +0800

    fer-frontend

---
+++ fer-frontend/config.py
@@ -0,0 +1,5 @@
+PROJECT_ID = 'fer-project-170115'
+CLOUD_STORAGE_BUCKET = 'fer-app-engine'
+MAX_CONTENT_LENGTH = 8 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+PREDICTION_SERVICE_URL = 'http://130.211.243.194:80'
